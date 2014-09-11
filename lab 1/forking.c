88b2c6f0b506a69876fdad24d185a3897c39b30b
David Greenberg <dgreenberg@grefin.com>
2014-09-11 17:32:03 -0400

    Edited makefile, changed all output to int output and cast, exchanged position of PID and PPID

--- lab 1/forking.c
+++ lab 1/forking.c
@@ -25,23 +25,23 @@ int main(int argc, char* argv){
   pid = fork();
   if(pid == 0){//Is the child
     //Print process id and parent id
-    printf("PID: %lu,PPID: %lu\n",getpid(),getppid());
+    printf("PPID: %d, PID: %d\n",(int)getppid(),(int)getpid());
 	 
 	
   }else if(pid==-1){
     perror("Failure occured. Could not create new fork."); 
   }else{ //is parent
   //Print the length of time since...
-    printf("Start: %jd\n",(intmax_t)currentTime);
-  pid_t retval;
+    printf("Start: %d\n",(int)currentTime);
+  pid_t childId;
   //Wait for child to finish
-  retval =  waitpid(pid ,&status,0);
+  childId =  waitpid(pid ,&status,0);
     //print PID, PPID, childId,return status
-  printf("PID: %lu,PPID: %lu, CID: %lu, RETVAL:%d\n",getpid(),getppid(),retval, status); 
+  printf("PPID: %d, PID: %d, CID: %d, RETVAL:%d\n",(int)getppid(),(int)getpid(),(int)childId, status); 
   //Print usertime, system time, cuser time, and csys time
     currentTime = times(&localtime);
-     printf("USER: %jd, SYS:%jd\nCUSER: %jd, CSYS: %jd\n",localtime.tms_utime,localtime.tms_stime,localtime.tms_cutime,localtime.tms_cstime);
-     printf("STOP: %jd\n",currentTime);
+     printf("USER: %d, SYS:%d\nCUSER: %d, CSYS: %d\n",(int)localtime.tms_utime,(int)localtime.tms_stime,(int)localtime.tms_cutime,(int)localtime.tms_cstime);
+     printf("STOP: %d\n",(int)currentTime);
   }
 
 //Report values
